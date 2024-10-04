f04f25d593e5d7fe1ebc8258f4d2ebe3a6b75690
Gidaxe <107138385+Gidaxe@users.noreply.github.com>
2024-10-04 06:20:18 +0000

    exercice nombres premiers ADJE Axel

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+'''
+Programme trouvant tous les nombres premiers de 0 a 100.
+'''
+
 from math import sqrt
 
 #### Fonction secondaire
 
 
-def isprime(p):
-
+def isprime(n):
     # votre code ici
+    '''
+    Retourne si le nombre n est premier ou pas.
 
-    pass
+    Args:
+        n: entier naturel.
+
+    Returns:
+        True: Booléen, si n est premier.
+        False: Booléen, si n n'est pas premier.
+    '''
+    if n in (0, 1):
+        return False
+    for i in range(2, int(sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    '''
+    Fonction principale
+    '''
 
     # vos appels à la fonction secondaire ici
 
